8339a2d52f8155e1cd844745f5b8e1bac6bcaf36
KannaKim <kimkanna18@gmail.com>
2019-01-30 02:56:03 -0600

    bugfixed

---
+++ LetterQuizBot/SensitiveData.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Principal;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LetterQuizBot
+{
+    class SensitiveData
+    {
+        public static string Token { get; set; }
+        public static string Id { get; set; }
+        public static string CommandPrefix { get; set; }
+
+        static SensitiveData()
+        {
+            Directory.SetCurrentDirectory(@"C:\Users\barkd\source\repos\LetterQuizBot\LetterQuizBot\");
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Token = Environment.GetEnvironmentVariable("LetterQuizBotToken");
+            Id = Environment.GetEnvironmentVariable("LetterQuizBotID");
+            using (StreamReader sr =  File.OpenText("config.json"))
+            {
+                string result = sr.ReadToEnd();
+                JToken json = JObject.Parse(result);
+                //Token = (string)json.SelectToken("token");
+                //Id = (string)json.SelectToken("ID");
+                CommandPrefix = JsonConvert.DeserializeObject<Dictionary<string,string>>(result)["command_prefix"];
+            } 
+        }
+    }
+}
