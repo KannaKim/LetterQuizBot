f661084b700b97c6505d522ae3f79ee9985555a6
KannaKim <kimkanna18@gmail.com>
2019-01-30 01:57:05 -0600

    first

---
+++ JaSonRelated/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JaSonRelated
+{
+    public class User
+    {
+        private Dictionary<string, Dictionary<string, dynamic>> user_data;
+
+
+
+    }
+}
