98717f905be5a01292bef7133a44692a7553cf17
KannaKim <kimkanna18@gmail.com>
2019-02-04 20:18:13 -0600

    log4net added for real

---
+++ LetterQuizBot/Loggers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterQuizBot
+{
+    public class Loggers
+    {
+        public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public Loggers()
+        { }
+    }
+}
